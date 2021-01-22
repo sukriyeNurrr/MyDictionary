6cedc3d22c91744fa5b954a93e0cc2364904b3e5
şükriye nur karaca <karaca.sukriye@hotmail.com>
2021-01-23 00:05:28 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "ilk ürün");
+            myDictionary.Add(2, "ikinci ürün");
+            myDictionary.Add(3, "Üçüncü ürün");
+        }
+    }
+}
